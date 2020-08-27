4b20eae9cc55176832543264c11aad5c260e6f21
Cristian Camilo Arenas <ccac1009@hotmail.com>
2020-08-26 21:41:28 -0500

    Se realiza la conexión entre la Main activity y el primer fragmento, además de la creación del primer fragmento (fragmento de login), con su correspondiente diseño

--- app/src/main/java/com/example/piggybank/MainActivity.java
+++ app/src/main/java/com/example/piggybank/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.piggybank;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            loginFragment();
+        }
+
+        private void loginFragment(){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, LoginFragment.newInstance());
+            fragmentTransaction.commit();
+
+        }
+
 }
